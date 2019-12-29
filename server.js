9bd9c4557a5559bcf36f81f054a60581e28318be
Yimmi Pernillo <yimmiss305@gmail.com>
2019-12-28 23:46:54 -0600

    Cambios en el package.json

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.json({
+    nombre:'Yimmi Daniel'
+  });
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
